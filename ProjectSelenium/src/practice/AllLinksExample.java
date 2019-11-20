fc8f6c82275ba0599a115d49faca4f1a2824c760
S@f' <S@f'@Sf-PC>
2019-11-20 11:07:32 +0530

    All Links code

---
+++ ProjectSelenium/src/practice/AllLinksExample.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AllLinksExample {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+		int count = 0;
+		System.out.println(allLinks.size());
+
+		// for loop
+
+		for (int i = 0; i < allLinks.size(); i++) {// 27
+			if (!allLinks.get(i).getText().equals("")) {// not empty
+				System.out.println(allLinks.get(i).getText());
+				count = count + 1;
+
+			}
+
+		}
+		System.out.println("Total no of links with text are ");
+		System.out.println(count);
+
+	}
+
+}
---
+++ ProjectSelenium/src/practice/ArrayExample.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4,5,6,7,78,98,4,6,6,6,6,6,4,4,45,4,5,5,5,5,5,3,3,3,5,4,5,46,6,6656,5,6};
+
+	
+		
+        System.out.println(arr.length);
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+		
+
+	}
+
+}
