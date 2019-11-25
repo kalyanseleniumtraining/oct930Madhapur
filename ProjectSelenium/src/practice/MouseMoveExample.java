d5fc3f66bb4e89aef7ef53077cd676fa272ceb39
S@f' <S@f'@Sf-PC>
2019-11-25 11:12:57 +0530

    25-11

--- ProjectSelenium/src/practice/MouseMoveExample.java
+++ ProjectSelenium/src/practice/MouseMoveExample.java
@@ -7,21 +7,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class MouseMoveExample {
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		WebDriver d = new FirefoxDriver();
 		d.get("http://ebay.com");
-		
-		WebElement myebay=d.findElement(By.linkText("My eBay"));
-		
-		Actions act=new Actions(d);
+
+		WebElement myebay = d.findElement(By.linkText("My eBay"));
+
+		Actions act = new Actions(d);
 		act.moveToElement(myebay).perform();
 		Thread.sleep(2000);
-		
-		WebElement messagesLink=d.findElement(By.linkText("Messages"));
-		
+
+		WebElement messagesLink = d.findElement(By.linkText("Messages"));
+
 		act.click(messagesLink).perform();
-	
+
 	}
 
 }
