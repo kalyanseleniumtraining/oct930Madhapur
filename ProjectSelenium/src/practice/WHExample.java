5dc2aad738f361ae15f825e4a18ee332cb021954
S@f' <S@f'@Sf-PC>
2019-11-26 11:01:06 +0530

    commit

---
+++ ProjectSelenium/src/practice/WHExample.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WHExample {
+
+	public void closeWindow(WebDriver d,String windowToClose) {
+		Set<String> ids = d.getWindowHandles();
+
+		for (String id : ids) {
+			d.switchTo().window(id);
+			if (d.getTitle().equals(windowToClose)) {
+				d.close();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		WebDriver d = new FirefoxDriver();
+		d.get("http://naukri.com");// 3 windows
+		WHExample obj=new WHExample();
+		obj.closeWindow(d, "Tech Mahindra");
+		obj.closeWindow(d, "LnT");
+
+	}
+
+}
