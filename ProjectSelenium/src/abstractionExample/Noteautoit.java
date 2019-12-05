8c8b2463ef5557e27a4a4f799e9dbb74b614bbae
S@f' <S@f'@Sf-PC>
2019-12-05 11:10:06 +0530

    css and autoit

---
+++ ProjectSelenium/src/abstractionExample/Noteautoit.java
@@ -0,0 +1,160 @@
+//AutoIT
+//------
+//
+//AutoIt is used to handle upload/download/print windows.
+//
+//First we need to download autoit from the below website
+//
+//https://www.autoitscript.com/site/autoit/downloads/
+//
+//Go to the above website and download 2 softwares
+//
+//1. AutoIt
+//2. Editor
+//
+//Install editor after AutoIT
+//
+//Go to Any Folder->right click->new AutoItV3 script
+//
+//"New AutoIt v3 Script.au3" file will be created
+//
+//Change the name of the file as per the requirement
+//
+//Right Click on the .au3 file click on Edit script
+//
+//Script window will be opened
+//
+//Remove all the comments in the script window and use the below
+//code to upload a file
+//
+//Sleep(2000);
+//
+//Send("C:\Users\S@f'\Desktop\testngoogle.txt")
+//Sleep(2000);
+//
+//Send("{Enter}");
+//Sleep(2000);
+//
+//
+//Save the file and right click on the file -> compile script X86
+//
+//an Exe file will be generated next to au3 file
+//
+//we should execute this exe file from selenium code to upload file
+//
+//
+//Runtime.getRuntime()
+//.exec("C:\\Users\\S@f'\\Desktop\\kalyan\\Upload.exe");
+//
+//Identification of objects using CSS Selector
+//--------------------------------------------
+//Using css selector we can use any attribute or any number of attributes
+//to identify the objects
+//
+//Example:
+//---------
+////[class='db'][tabindex='1'][type='text']
+//
+//Difference between xpath and css selector
+//------------------------------------------
+//
+//Using xpath we can use index but css selector does not provide index
+//
+//Xpath is lengthy and css selector is compact
+//
+//
+//Difference between selenium 2.x and 3.x
+//-----------------------------------------
+//
+//To work with firefox browser we no need to download the driver
+//server exe file, it will be available in selenium webdriver 2.53.1
+//jar files
+//
+//selenium will automatically install the exe file in to the browser
+//
+//due to security problems selenium stopped installing the exe file
+//in tothe firefox browser automatically
+//
+//In selenium 3.x we should download firefoxdriver exe file and copy
+//to the project manually
+//
+//Reuiq
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ ProjectSelenium/src/practice/AutoITExample.java
@@ -0,0 +1,35 @@
+package practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AutoITExample {
+	
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+//		driver.get("http://www.convertfiles.com/");
+//		
+//		WebElement browseButton=driver.findElement(By.name("uploadedfile"));
+//		browseButton.click();
+//		//code to execute the exe file from java
+//		
+//		Runtime.getRuntime().exec("C:\\Users\\S@f'\\Desktop\\kalyan\\Upload.exe");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
